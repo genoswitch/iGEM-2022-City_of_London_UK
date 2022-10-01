7e9909f1b699c60fc36626e0e2134f98578fc528
James <jcahill773@gmail.com>
2022-10-01 08:15:29 +0100

    Change columns to fit design

--- src/components/Columns.tsx
+++ src/components/Columns.tsx
@@ -6,10 +6,11 @@ import Col from 'react-bootstrap/Col';
 
 
 import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
 
 class ColumnEntry extends React.Component {
     static defaultProps = {
-        title: "Placeholder Title",
+        title: "Placeholder",
         bg_col: "#ffffff",
         col: "#000000"
     }
@@ -27,18 +28,19 @@ class ColumnEntry extends React.Component {
 }
 
 const Columns = () => {
-    return (<Container>
-        <Row>
-            <Col><ColumnEntry title="Meet the Team" col="#d4e6f1" bg_col="#ae997a" /></Col>
-            <Col><ColumnEntry title="Methodology" col="#4c5b61" bg_col="#e0ccd8" /></Col>
-            <Col><ColumnEntry title="An Introductory Guide" col="#fbf7e4" bg_col="#90bbc4" /></Col>
-        </Row>
-        <Row>
-            <Col><ColumnEntry col="#485c52" bg_col="#ced5de" /></Col>
-            <Col><ColumnEntry col="#4e426b" bg_col="#b7c1b8" /></Col>
-            <Col><ColumnEntry title="People" col="#f7f1e2" bg_col="#ce7984" /></Col>
-        </Row>
-    </Container >)
+    return (<div>
+        <CardGroup style={{ "height": "200px" }}>
+            <ColumnEntry title="Meet the Team" col="#d4e6f1" bg_col="#ae997a" />
+            <ColumnEntry title="Methodology" col="#4c5b61" bg_col="#e0ccd8" />
+            <ColumnEntry title="An Introductory Guide" col="#fbf7e4" bg_col="#90bbc4" />
+        </CardGroup>
+        <CardGroup style={{ "height": "200px" }}>
+            <ColumnEntry col="#485c52" bg_col="#ced5de" />
+            <ColumnEntry col="#4e426b" bg_col="#b7c1b8" />
+            <ColumnEntry title="People" col="#f7f1e2" bg_col="#ce7984" />
+        </CardGroup>
+    </div>)
+    //return (<CardGroup><Card>s</Card><Card>s</Card></CardGroup>)
 };
 
 export default Columns
