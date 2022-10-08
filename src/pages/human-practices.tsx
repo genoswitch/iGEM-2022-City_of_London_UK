bc1aed6076530e0eb65e1ad1b5c5414a3ea853d7
James Cahill <jcahill773@gmail.com>
2022-10-08 13:01:00 +0100

    Create human practices page

---
+++ src/pages/human-practices.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+import MarkdownPage from "../components/MarkdownPage"
+
+import data from "../../pages/human-practices.md"
+
+const Page = () => {
+    return (<MarkdownPage data={data} title="Human Practices" />)
+}
+
+export default Page
