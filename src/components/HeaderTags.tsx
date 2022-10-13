5d6805f69481633c58f7c62a9996bdf56ab82da4
James Cahill <jcahill773@gmail.com>
2022-10-13 15:32:03 +0100

    Add favicons, webmanifest

--- src/components/HeaderTags.tsx
+++ src/components/HeaderTags.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
 
+import { withPrefix } from 'gatsby';
+
 class HeaderTags extends React.Component {
   static defaultProps = {
     title: 'aLFA REMASK',
@@ -10,6 +12,24 @@ class HeaderTags extends React.Component {
     return (
       <Helmet>
         <title>{this.props.title}</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="https://static.igem.wiki/teams/4508/wiki/site-res/icons/apple-touch-icon.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="https://static.igem.wiki/teams/4508/wiki/site-res/icons/favicon-32x32.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="https://static.igem.wiki/teams/4508/wiki/site-res/icons/favicon-16x16.png"
+        ></link>
+        <link rel="manifest" href={withPrefix('site.webmanifest')}></link>
       </Helmet>
     );
   }
